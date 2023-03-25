b9efe5a5a97fcf9d81619e4d897b81b30b043589
BTheofil <btheofil7@gmail.com>
2023-03-25 17:28:18 +0100

    fix api<33 notification bug versions up

--- app/src/main/java/hu/tb/tasky/ui/add_edit_task/alarm/AlarmReceiver.kt
+++ app/src/main/java/hu/tb/tasky/ui/add_edit_task/alarm/AlarmReceiver.kt
@@ -4,16 +4,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.annotation.RequiresApi
 import hu.tb.tasky.model.Task
 import hu.tb.tasky.ui.add_edit_task.alarm.AlarmScheduler.Companion.INTENT_EXPIRE_TASK_KEY
 import hu.tb.tasky.ui.notification.TaskNotificationService
 
 class AlarmReceiver: BroadcastReceiver() {
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onReceive(context: Context?, intent: Intent?) {
-        val expireTask = intent?.getParcelableExtra(INTENT_EXPIRE_TASK_KEY, Task::class.java) ?: return
+        val expireTask = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(INTENT_EXPIRE_TASK_KEY, Task::class.java) ?: return
+        } else {
+            intent?.getParcelableExtra(INTENT_EXPIRE_TASK_KEY) ?: return
+        }
         if (context != null) {
             val service = TaskNotificationService(context)
             service.showNotification(expireTask)
